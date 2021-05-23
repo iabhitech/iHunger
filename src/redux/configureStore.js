2cc386798b1f77f4fc17e1bc0d910f6986a57565
Abhineet Verma <198.abhineet@gmail.com>
2021-05-23 21:28:38 +0530

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,17 @@
-import { createStore } from "redux";
-import { initialState, Reducer } from "./reducer";
+import { combineReducers, createStore } from "redux";
+import { Comments } from "./comments";
+import { Dishes } from "./dishes";
+import { Leaders } from "./leaders";
+import { Promotions } from "./promotion";
 
 export const ConfigureStore = () => {
-  const store = createStore(Reducer, initialState);
+  const store = createStore(
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders,
+    })
+  );
   return store;
 };
